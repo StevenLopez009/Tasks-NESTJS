7edcc26c33f8cfeccd5a6b226dac71fe7fe5f866
Steven Lopez Mancera <150303550+StevenLopez009@users.noreply.github.com>
2026-01-03 01:41:26 -0500

    creacion del proyecto y conexion a mongodb

---
+++ tasksapi/src/schemas/task.schema.ts
@@ -0,0 +1,16 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({
+  timestamps: true,
+})
+
+export class Task {
+  @Prop({ required: true, trim: true })
+  title: string;
+  @Prop({ trim: true, })
+  description: string;
+  @Prop({ default: false })
+  completed: boolean;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
---
+++ tasksapi/src/tasks/tasks.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from 'src/schemas/task.schema';
+import { CreateTaskDto } from 'src/dto/create-task.dto';
+import { UpdateTaskDto } from 'src/dto/update-task.dto';
+
+
+@Injectable()
+export class TasksService {
+  constructor(@InjectModel('Task') private taskModel: Model<Task>
+  ) { }
+
+  async findAll(): Promise<Task[]> {
+    return this.taskModel.find();
+  }
+
+  async create(createTask: CreateTaskDto): Promise<Task> {
+    const newTask = new this.taskModel(createTask);
+    return await newTask.save();
+  }
+
+  async findOne(id: string) {
+    return this.taskModel.findById(id);
+  }
+
+  async delete(id: string) {
+    return this.taskModel.findByIdAndDelete(id);
+  }
+
+  async update(id: string, updateTask: UpdateTaskDto) {
+    return this.taskModel.findByIdAndUpdate(id, updateTask, { new: true });
+  }
+}
